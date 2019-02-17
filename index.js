e505e77f7497d4c1b91b8c9d3f9b7e0b4283854e
Reavershark <jonasty1999@gmail.com>
2019-02-17 14:07:11 +0100

    Add TTN example

--- index.js
+++ index.js
@@ -23,3 +23,24 @@ server.on('ready', setup);
 function setup() {
     console.log('Mosca broker is up and running')
 }
+
+
+// The Things Network
+require('dotenv').config()
+var ttn = require("ttn")
+var appID = process.env.APP_ID
+var accessKey = process.env.ACCESS_KEY
+
+if (appID && accessKey) {
+    ttn.data(appID, accessKey)
+        .then(function (client) {
+            client.on("uplink", function (devID, payload) {
+                console.log("Received uplink from ", devID)
+                console.log(payload)
+            })
+        })
+        .catch(function (error) {
+            console.error("Error", error)
+            process.exit(1)
+        })
+}
