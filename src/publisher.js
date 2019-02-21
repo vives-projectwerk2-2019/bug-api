6ebad16548f8d5f6295fe653aa18d2cfb9cca858
JelleDBZ <jelle.debuyzere@hotmail.be>
2019-02-21 15:21:14 +0100

    Added example JSON string for input events users

--- src/publisher.js
+++ src/publisher.js
@@ -1,6 +1,24 @@
 var mqtt = require('mqtt');
 
 var client  = mqtt.connect('mqtt://127.0.0.1'); // ip van de server waarop de broker zal staan
+
+//JSON string voor input events example
+var dataInput = {
+    Player: {
+        name: "Jelle",
+        id: 1, 
+        input: "button", // don't know what this will be yet
+        joined: true
+    },
+    Controller: {
+        id: null,
+        addons: ["rocket", "speedboost", "anotherthing"]
+    }
+};
+
+//JSON string voor player update 
+
+
 client.on('connect', function () {
 
     setInterval(function() {
