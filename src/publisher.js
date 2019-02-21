07988b10986003fbef732ece2537223a9ab15bb8
Reavershark <jonasty1999@gmail.com>
2019-02-21 15:55:51 +0100

    Add database example to publisher

--- src/publisher.js
+++ src/publisher.js
@@ -1,4 +1,5 @@
 var mqtt = require('mqtt');
+var db = require('./db');
 
 var client  = mqtt.connect('mqtt://127.0.0.1'); // ip van de server waarop de broker zal staan
 
@@ -24,5 +25,26 @@ client.on('connect', function () {
     setInterval(function() {
         client.publish('API', 'Hello mqtt'); TODO://publish to game server in JSON format
         console.log('Publisher: Message Sent');
+
+        // Database example
+        db.pool.getConnection()
+            .then(conn => {
+              console.log("connected ! connection id is " + conn.threadId);
+
+              conn.query("INSERT INTO users (username, password) VALUES (\"api\", \"api\")")
+                .then(rows => {
+                  console.log(rows);
+                });
+              conn.query("SELECT username, password FROM users")
+                .then(rows => {
+                  console.log(rows);
+                });
+
+              conn.end(); //release to pool
+            })
+            .catch(err => {
+              console.log("not connected due to error: " + err);
+            });
+
     }, 1000);
 });
