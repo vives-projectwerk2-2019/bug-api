28761d27e4ba26d506e9d70aba5654f8dd4f0582
JelleDBZ <jelle.debuyzere@hotmail.be>
2019-02-23 21:47:59 +0100

    Fixed bug where username wasn't displayed

--- src/client.js
+++ src/client.js
@@ -15,16 +15,16 @@ var dataObject = {
         addons: ["rocket", "speedboost", "anotherthing"]
     }
 };
-var myDataObj = JSON.stringify(dataObject); // ready to be send as JSON 
-
 //This will subscribe the client on TTN and publish the right JSON object to game server!
 
 client.on('connect', function () {
     setInterval(() => {
         client.subscribe('TTN', function(err){ //subscribe op TTN
             if(!err){
-                client.publish('game', myDataObj);
-                console.log("Publisher: " + myDataObj);
+                dataObject.Player.username = "LOL";
+                dataObject.Player.button = ttndata;
+                client.publish('game', JSON.stringify(dataObject));
+                console.log("Publisher: " + JSON.stringify(dataObject));
             }
         }) 
     }, 1000);
@@ -33,6 +33,6 @@ client.on('connect', function () {
 client.on('message', function (topic, message) {
     //TODO:how will the format be? How to process information?
     ttndata = message.toString(); //message will be a JSON string need to parse, format will be {button:2, dev_id: 3}
-    dataObject.Player.username = "Jelle";
+    
     console.log("Subscriber: Button: " + ttndata);
 })
