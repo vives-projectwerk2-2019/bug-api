e81066039e3c31736e354a6ceb690a4b1e644f9b
JelleDBZ <jelle.debuyzere@hotmail.be>
2019-02-25 13:53:33 +0100

    Deleted unused lines from unused files

--- server.js
+++ server.js
@@ -1,9 +1,5 @@
 // Read .env
 require('dotenv').config()
 
-// Express router
-var router = require('./src/router')
-
 // Mqtt clients
-var publisher = require('./src/publisher')
 var client = require('./src/client.js')
