2c99ac8ed61b61f34df2ef621428413049b9f352
unknown <jelle.debuyzere@student.vives.be>
2019-03-14 22:42:53 +0100

    Added some Fetch API examples for later use

--- src/http_request.js
+++ src/http_request.js
@@ -1,21 +1,33 @@
-var http = require('http');
+//var http = require('http');
+
 var urldata = {
-   host: 'http://localhost:8000/api/user',
+   host: 'http://localhost:8000/api/user', // need an API token
    path: '/acsearch',
    method: 'GET'
 }
+/* THIS WILL SEND A HUGE STRING */
 
-function OnResponse(response) { 
- var data = ''; 
-   response.on('data', function(chunk) { 
-        data += chunk; 
-   });
+// function OnResponse(response) { 
+//  var data = ''; 
+//    response.on('data', function(chunk) { 
+//         data += chunk; 
+//    });
 
-    response.on('end', function() {
-        console.log(data);
-    });
-   }
+//     response.on('end', function() {
+//         console.log(data);
+//     });
+//    }
 
-   fetch(urldata)
+//    fetch(urldata)
 
- http.request(urldata, OnResponse).end();
+//  http.request(urldata, OnResponse).end();
+
+ /* How to get a user with given ID */
+fetch('http://localhost:8000/api/user')
+  .then(response => response.json())
+  .then(data => console.log(data))
+
+  /* Get All users */
+fetch('http://localhost:8000/api/user')
+   .then(response => response.json())
+   .then(data => console.log(data))
