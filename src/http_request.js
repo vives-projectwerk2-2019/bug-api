124d5fa0fd0cd25d31dc77ca067a51ccf8a5acf0
unknown <jelle.debuyzere@student.vives.be>
2019-03-31 16:59:23 +0200

    Removed some comments

--- src/http_request.js
+++ src/http_request.js
@@ -5,29 +5,9 @@ var urldata = {
    path: '/acsearch',
    method: 'GET'
 }
-/* THIS WILL SEND A HUGE STRING */
 
-// function OnResponse(response) { 
-//  var data = ''; 
-//    response.on('data', function(chunk) { 
-//         data += chunk; 
-//    });
-
-//     response.on('end', function() {
-//         console.log(data);
-//     });
-//    }
-
-//    fetch(urldata)
-
-//  http.request(urldata, OnResponse).end();
-
+/* This will be used to get the right user from the kiosk */
 //  /* How to get a user with given ID */
 // fetch('http://localhost:8000/api/user')
 //   .then(response => response.json())
 //   .then(data => console.log(data))
-
-//   /* Get All users */
-// fetch('http://localhost:8000/api/user')
-//    .then(response => response.json())
-//    .then(data => console.log(data))
