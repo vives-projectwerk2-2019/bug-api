1b74ae6505e26a2f4767a04d975ab9e92c481323
JelleDBZ <jelle.debuyzere@hotmail.be>
2019-04-02 21:02:54 +0200

    Added comment for the url

--- src/http_request.js
+++ src/http_request.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch');
 /* This will be used to get the right user from the kiosk */
  /* How to get a user with given ID */
 module.exports = async() => {
-   const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+   const response = await fetch('https://jsonplaceholder.typicode.com/todos/1'); //test url
    const json = await response.json();
    return json;
 }
