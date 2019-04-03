c09e9e8b0bde5a323878bb1c316ecbb4c8d79c9f
unknown <jelle.debuyzere@student.vives.be>
2019-04-03 22:02:40 +0200

    Implemented http post request with data dev_id (url still needs to change to API endpoint)

--- src/fetch.js
+++ src/fetch.js
@@ -1,8 +1,19 @@
 const fetch = require('node-fetch');
+const devid = require('./client');
+
+var url = 'https://jsonplaceholder.typicode.com/todos/1'; //needs url from API endpoint
 
 /* This will be used to get the right user from the kiosk */
 module.exports = async() => {
-   const response = await fetch('https://jsonplaceholder.typicode.com/todos/1'); //test url, still need a token for real url
+   const response = await fetch(url, { 
+      method: "POST",
+      credentials: "same-origin",
+      headers: {
+         "Content-type": "application/json",
+      },
+      body: JSON.stringify(devid), //needs to get dev_id
+
+   }); 
    const json = await response.json();
    return json;
 }
