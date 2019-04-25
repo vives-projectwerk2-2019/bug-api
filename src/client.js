8e44276b9bab62607ce0c5c96d124a3340a43e0b
JelleDBZ <jelle.debuyzere@hotmail.be>
2019-04-25 19:07:27 +0200

    Added temporary solution for id, not using hardcoded anymore

--- src/client.js
+++ src/client.js
@@ -2,6 +2,7 @@ var mqtt = require("mqtt");
 var client = mqtt.connect("mqtt://" + process.env.BROKER_HOST);
 let http = require("./fetch");
 const processdata = require("./dataprocessor");
+var user_id;
 
 //This will subscribe the client on ttn and publish the right JSON object to game server!
 client.on("connect", function() {
@@ -11,10 +12,12 @@ client.on("connect", function() {
 
 client.on("message", async (topic, message) => {
   var ttndata = JSON.parse(message.toString()); //parsing
+  if (ttndata.id) {
+    user_id = ttndata.id;
+  }
 
   var p = new processdata();
-  var httpdata = await http.httpids(ttndata.dev_id, ttndata.id); //fetching data
-
+  var httpdata = await http.httpids(ttndata.dev_id, user_id); //fetching data
   var dataobj = p.checkAndProcess(ttndata, topic, httpdata);
 
   client.publish("game", JSON.stringify(dataobj));
