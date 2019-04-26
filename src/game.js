06e1239edb6d134a75ed5206d1d6fdde13185e98
JelleDBZ <jelle.debuyzere@hotmail.be>
2019-04-26 16:56:36 +0200

    Added game class that makes the object that will be send to the game topic

---
+++ src/game.js
@@ -0,0 +1,25 @@
+
+class Game {
+  constructor(client) {
+    this.client = client;
+  }
+  publishPlayerAction(controller, movement, actions) {
+    var dataObject = {
+      Player: {
+        username: controller.playerName,
+        movement: movement,
+        dev_id: controller.dev_id,
+        action: actions
+      },
+      Controller: {
+        id: controller.uid,
+        addons: controller.addons,
+        dev_id: controller.dev_id
+      }
+    };
+
+    this.client.publish("game3", JSON.stringify(dataObject));
+    console.log("Publisher: " + JSON.stringify(dataObject));
+  }
+}
+module.exports = Game;
