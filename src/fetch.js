d5f6d0b83639c1adc3fc4b4cb0d64c41c4e31d81
JelleDBZ <jelle.debuyzere@hotmail.be>
2019-04-26 13:56:23 +0200

    Added try catch to get request

--- src/fetch.js
+++ src/fetch.js
@@ -3,16 +3,21 @@ const fetch = require("node-fetch");
 /* This will be used to get the right user from the kiosk */
 const httpids = async (dev_id, user_dongle_id) => {
   var url = `http://localhost:8000/api/session/${dev_id}/${user_dongle_id}`;
-  const response = await fetch(url, {
-    method: "GET",
-    credentials: "same-origin",
-    headers: {
-      "Content-type": "application/json"
-    }
-  });
-
-  const json = await response.json();
-  return json;
+  try {
+    const response = await fetch(url, {
+      method: "GET",
+      credentials: "same-origin",
+      headers: {
+        "Content-type": "application/json"
+      }
+    });
+  
+    const json = await response.json();
+    return json;
+  }
+  catch (error){
+    console.log(error);
+  }
 };
 
 /* Addons door sturen naar kiosk */
